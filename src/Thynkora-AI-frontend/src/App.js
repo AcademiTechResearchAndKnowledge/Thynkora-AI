3f5cf4ce97d180562d865ddd748cb4c7f3f60d15
Jokuma <joshuamanzarate@gmail.com>
2025-07-23 13:46:49 +0800

    Initial commit for Thynkora-AI project

--- src/Thynkora-AI-frontend/src/App.js
+++ src/Thynkora-AI-frontend/src/App.js
@@ -45,7 +45,7 @@ const App = () => {
     if (showLanding) {
         return (React.createElement("div", { className: "landing-wrapper", style: {
                 width: '100%',
-                minHeight: '200vh',
+                minHeight: '300vh',
                 overflow: 'auto',
                 backgroundImage: `url(${landingBg})`,
                 backgroundSize: '100% auto', // or '100% auto' if needed
