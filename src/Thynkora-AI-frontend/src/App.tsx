e14ca620b4b7eb2d0515e3d3044c12b96f511f47
Jokuma <joshuamanzarate@gmail.com>
2025-07-23 22:21:07 +0800

    Initial commit for Thynkora-AI project

--- src/Thynkora-AI-frontend/src/App.tsx
+++ src/Thynkora-AI-frontend/src/App.tsx
@@ -203,7 +203,32 @@ const App: React.FC = () => {
           }}
         >
         </button>
-
+        {/*    CUSTOMER SUPPORT   */}
+        <button
+          onClick={() => {
+            setShowLanding(false),
+              setAboutUsPage(false),
+              setShowp1(false),
+              setPTSPage(false),
+              setCustomerSupportPage(true),
+              setArticlesPage(false)
+          }}
+          style={{
+            position: 'absolute',
+            bottom: '1rem',
+            left: '50%',
+            transform: 'translateX(197%)',
+            padding: '1rem 2.5rem',
+            fontSize: '1.2rem',
+            backgroundColor: 'rgba(0, 0, 0, 100)',
+            color: '#000',
+            border: 'none',
+            borderRadius: '8px',
+            cursor: 'pointer',
+            zIndex: 10,
+          }}
+        >
+        </button>
 
 
 
@@ -289,32 +314,7 @@ const App: React.FC = () => {
           }}
         >
         </button>
-        {/*    CUSTOMER SUPPORT   */}
-        <button
-          onClick={() => {
-            setShowLanding(false),
-              setAboutUsPage(false),
-              setShowp1(false),
-              setPTSPage(false),
-              setCustomerSupportPage(true),
-              setArticlesPage(false)
-          }}
-          style={{
-            position: 'absolute',
-            bottom: '1rem',
-            left: '50%',
-            transform: 'translateX(197%)',
-            padding: '1rem 2.5rem',
-            fontSize: '1.2rem',
-            backgroundColor: 'rgba(0, 0, 0, 100)',
-            color: '#000',
-            border: 'none',
-            borderRadius: '8px',
-            cursor: 'pointer',
-            zIndex: 10,
-          }}
-        >
-        </button>
+
 
 
 
